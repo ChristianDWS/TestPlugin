8c85d3e3a192bebe2ef74e81acfa051bbbb9644e
Christian Stangerup <christian.stangerup@icloud.com>
2022-07-09 11:21:43 +0200

    Implementing a custom configuration file

--- src/main/java/me/tyloon1e/testplugin/TestPlugin.java
+++ src/main/java/me/tyloon1e/testplugin/TestPlugin.java
@@ -3,21 +3,50 @@ package me.tyloon1e.testplugin;
 import me.tyloon1e.testplugin.commands.VersionCommand;
 import me.tyloon1e.testplugin.commands.SunCommand;
 import me.tyloon1e.testplugin.commands.TestCommand;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public class TestPlugin extends JavaPlugin {
 
+    public static TestPlugin plugin;
+    private File customConfigFile;
+    private FileConfiguration customConfig;
+
     @Override
     public void onEnable() {
-        getLogger().info("TestPlugin has been enabled, woo!");
+        plugin = this;
+
+        createCustomConfig();
 
         this.getCommand("testplugin").setExecutor(new TestCommand());
         this.getCommand("testversion").setExecutor(new VersionCommand());
         this.getCommand("testsun").setExecutor(new SunCommand());
+
+        getLogger().info("TestPlugin has been enabled, woo!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("TestPlugin has been disabled. So sad.");
+    }
+
+    public FileConfiguration getCustomConfig() {
+        return this.customConfig;
+    }
+
+    // creates a "custom.yml" config file (if not already present)
+    private void createCustomConfig() {
+        customConfigFile = new File(getDataFolder(), "custom.yml");
+
+        if (!customConfigFile.exists()) {
+            customConfigFile.getParentFile().mkdirs();
+            saveResource("custom.yml", false); // copies file from resources folder in JAR
+        }
+
+        customConfig = new YamlConfiguration();
+        YamlConfiguration.loadConfiguration(customConfigFile); // loads configuration and handles possible errors
     }
 }
--- src/main/java/me/tyloon1e/testplugin/commands/VersionCommand.java
+++ src/main/java/me/tyloon1e/testplugin/commands/VersionCommand.java
@@ -1,13 +1,17 @@
 package me.tyloon1e.testplugin.commands;
 
+import me.tyloon1e.testplugin.TestPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 public class VersionCommand implements CommandExecutor {
+
+    FileConfiguration customConfig = TestPlugin.plugin.getCustomConfig();
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -22,9 +26,8 @@ public class VersionCommand implements CommandExecutor {
                 return true;
             }
 
-            String ver = Bukkit.getServer().getBukkitVersion();
-
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eThe server is running " + ver));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eThe server is running " + Bukkit.getServer().getBukkitVersion()));
+            sender.sendMessage("The result of key 'test-option' is " + customConfig.getBoolean("test-option"));
         }
 
         return false;
